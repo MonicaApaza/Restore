d4840a22cf18005089e18ed643a0ddbd3006a932
Monik <monica.apazacora@gmail.com>
2023-01-05 18:40:02 -0400

    adding darkmode

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,16 +1,23 @@
-import { Container } from "@mui/material";
+import { Container, createTheme, ThemeProvider } from "@mui/material";
+import { useState } from "react";
 import Catalog from "../../features/catalog/Catalog";
 import Header from "./Header";
 
 function App() {
-
+  const [darkMode, setDarkMode]= useState(false);
+  const palettType = darkMode ? 'dark' : 'light';
+  const theme = createTheme({
+    palette:{
+      mode: palettType
+    }
+  })
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Header />
       <Container>
         <Catalog/>
       </Container>
-    </>
+    </ThemeProvider>
   );
 }
 
--- client/src/features/catalog/ProductList.tsx
+++ client/src/features/catalog/ProductList.tsx
@@ -8,7 +8,6 @@ interface Props {
 export default function ProductList({ products }: Props) {
     return (
         <Grid container spacing={4}>
-
             {products.map(product => (
                 <Grid item xs={3} key={product.id}>
                     <ProductCard product={product} />
