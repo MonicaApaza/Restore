8d48a5c070bcbd7166712febbffe3d7140b95899
Monik <monica.apazacora@gmail.com>
2023-01-05 18:34:54 -0400

    removing add product button

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,4 +1,3 @@
-import { Button } from "@mui/material";
 import { useEffect, useState } from "react";
 import { Product } from "../../app/models/product"
 import ProductList from "./ProductList";
@@ -12,22 +11,9 @@ export default function Catalog() {
             .then(data => setProducts(data))
     }, [])
 
-    function addProduct() {
-        setProducts(currentProducts => currentProducts.concat(
-            {
-                id: currentProducts.length + 101,
-                name: 'product' + (currentProducts.length + 1),
-                price: 100.00 * (currentProducts.length + 1),
-                brand: 'some brand',
-                description: 'some desc',
-                pictureUrl: 'http://picsum.photos/' + 10.00 * (currentProducts.length)
-
-            }))
-    }
     return (
         <>
             <ProductList products={products}></ProductList>
-            <Button variant="contained" onClick={addProduct}>Add Producto</Button>
         </>
     );
 }
