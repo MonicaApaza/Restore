8dcdb9b9cd1550ac080c4017ad930d04a8160b5e
Monik <monica.apazacora@gmail.com>
2023-01-13 13:24:54 -0400

    formating errors response, adding ExceptionMiddleware

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -18,7 +18,7 @@ namespace API.Controllers
         [HttpGet("bad-request")]
         public ActionResult GetBadRequest()
         {
-            return BadRequest("This is a bad request");
+            return BadRequest( new ProblemDetails{Title="this is a bad request"});
         }
 
         [HttpGet("unauthorized")]
