c91ff1afc3e03d645b4354a3f4ffe20e26a7d075
Monik <monica.apazacora@gmail.com>
2023-01-17 18:38:16 -0400

    adding agent to centralize api calls

--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -2,6 +2,7 @@ import { Divider, Grid, Table, TableBody, TableCell, TableContainer, TableRow, T
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import agent from "../../app/api/agent";
 import { Product } from "../../app/models/product";
 
 export default function ProductDetails() {
@@ -11,13 +12,10 @@ export default function ProductDetails() {
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        axios.get(`http://localhost:5000/api/Products/${id}`)
-            .then(response => setProduct(response.data))
-            .catch(error => {
-                console.log(error);
-                setLoading(false)
-            })
-            .finally(() => setLoading(false))
+        agent.Catalog.details(parseInt(id))
+                     .then(response => setProduct(response))
+                     .catch(error => console.log(error))
+                     .finally(() => setLoading(false))
     }, [id]);
 
     if (loading)
@@ -38,12 +36,12 @@ export default function ProductDetails() {
         // </Typography>
         <Grid container spacing={6}>
             <Grid item xs={6}>
-                <img src={product.pictureUrl} alt={product.name} style={{width:'100%'}}></img>
+                <img src={product.pictureUrl} alt={product.name} style={{ width: '100%' }}></img>
             </Grid>
             <Grid item xs={6}>
                 <Typography variant="h3">{product.name}</Typography>
-                <Divider sx={{mb:2}}></Divider>
-                <Typography variant="h4" color='secondary'>${(product.price/100).toFixed(2)}</Typography>
+                <Divider sx={{ mb: 2 }}></Divider>
+                <Typography variant="h4" color='secondary'>${(product.price / 100).toFixed(2)}</Typography>
                 <TableContainer>
                     <Table>
                         <TableBody>
