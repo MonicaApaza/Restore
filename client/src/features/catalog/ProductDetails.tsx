5f4adf45f446113ea5ee4643df88448245150456
Monik <monica.apazacora@gmail.com>
2023-01-19 13:11:13 -0400

    adding axios interceptors

--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -1,5 +1,4 @@
 import { Divider, Grid, Table, TableBody, TableCell, TableContainer, TableRow, Typography } from "@mui/material";
-import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import agent from "../../app/api/agent";
@@ -14,7 +13,7 @@ export default function ProductDetails() {
     useEffect(() => {
         agent.Catalog.details(parseInt(id))
                      .then(response => setProduct(response))
-                     .catch(error => console.log(error.response))
+                     .catch(error => console.log(error))
                      .finally(() => setLoading(false))
     }, [id]);
 
